111baf29fe460092a9a7db581c82ea4325bc2c3d
DenisPapushaJava <denispapusha0@gmail.com>
2023-10-12 17:51:41 +0300

    0.1

--- src/view/App/pages/sign-up.jsx
+++ src/view/App/pages/sign-up.jsx
@@ -1,29 +1,45 @@
+import { useForm } from 'react-hook-form';
 import styles from './style.module.scss';
 import { Link } from 'react-router-dom';
-import { Input, Checkbox, Button } from 'antd';
+import { Button } from 'antd';
 
-import * as Yup from 'yup';
+
 
 const SignUp = () => {
-  const checkInput = Yup.object().shape({
-    email: Yup.string().required('Поле "Email" должно быть заполнено').email('Email не верный'),
-    password: Yup.string()
-      .min(6, 'Поле "Password" не должно содержать менее 6 символов')
-      .max(20, 'Поле "Password" не должно содержать более 40 символов')
-      .required('Поле "Password" должно быть заполнено'),
-  });
-  console.log(checkInput);
+  // const checkInput = Yup.object().shape({
+  //   email: Yup.string().required('Поле "Email" должно быть заполнено').email('Email не верный'),
+  //   password: Yup.string()
+  //     .min(6, 'Поле "Password" не должно содержать менее 6 символов')
+  //     .max(20, 'Поле "Password" не должно содержать более 40 символов')
+  //     .required('Поле "Password" должно быть заполнено'),
+  // });
+  const {
+    register,
+    formState: {
+      errors,
+    },
+    handleSubmit,
+    reset,
+  } = useForm();
+  const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
+  const onSubmit = (data) => {
+    console.log(data);
+    reset();
+  };
+
   return (
     <div className={styles.container}>
       <h3 className={styles.containerTitle}>
         Create new account
       </h3>
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <label className={styles.containerLabel}>
           <p className={styles.containerInputName}>
             Username
           </p>
-          <input placeholder='Username' />
+          <input placeholder='Username'
+                 Хююю
+          />
         </label>
         <label className={styles.containerLabel}>
           <p className={styles.containerInputName}>
@@ -49,6 +65,7 @@ const SignUp = () => {
         </label>
         <Button type='primary'
                 className={styles.containerButton}
+                htmlType='submit'
                 size='large'
         >Create</Button>
         <p className={styles.containerFooter}>
