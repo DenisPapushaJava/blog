e47abf6df8f8b27b472d669f6d4b4506dc778c87
DenisPapushaJava <denispapusha0@gmail.com>
2023-10-19 16:59:43 +0300

    fix sing-up

--- src/view/App/components/hoc/authentication.jsx
+++ src/view/App/components/hoc/authentication.jsx
@@ -1,5 +1,6 @@
 import { Navigate, useLocation } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import {PATH} from "../../../../constans/index.js";
 
 
 const Authentication = ({ children }) => {
@@ -7,7 +8,7 @@ const Authentication = ({ children }) => {
   const isLogin = useSelector((state) => state.user.isLogin);
 
   if (!isLogin) {
-    return <Navigate to={'/sign-in'} state={{ from: location }} />;
+    return <Navigate to={PATH.singIn} state={{ from: location }} />;
 
   }
   return children;
